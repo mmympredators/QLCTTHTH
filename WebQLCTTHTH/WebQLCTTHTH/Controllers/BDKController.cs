083e1056b7b27698515fa246d7c56534c183ab95
SynG <mmympredators@gmail.com>
2018-05-09 15:33:38 +0700

    QLCTTHTH_Funtion_ver1.3
    Add QLVT
    Add QLNTT
    Add QLGT

--- WebQLCTTHTH/WebQLCTTHTH/Controllers/BDKController.cs
+++ WebQLCTTHTH/WebQLCTTHTH/Controllers/BDKController.cs
@@ -3,86 +3,338 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebQLCTTHTH.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
 
 namespace WebQLCTTHTH.Controllers
 {
     public class BDKController : Controller
     {
+        QLCTTHTHDataContext db = new QLCTTHTHDataContext();
         // GET: BDK
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Details(int id)
+        public ActionResult DT(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+
+            return View(db.DotThis.ToList().OrderBy(n => n.LanThi).ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpGet]
+        public ActionResult DTCreate()
         {
             return View();
         }
 
-        // GET: Default/Create
-        public ActionResult Create()
+        public ActionResult DTDetails(int id)
         {
-            return View();
+            DotThi dt = db.DotThis.SingleOrDefault(n => n.LanThi == id);
+            ViewBag.LanThi = dt.LanThi;
+            if (dt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(dt);
         }
 
-        // POST: Default/Create
+        public ActionResult DTDelete(int id)
+        {
+            DotThi dt = db.DotThis.SingleOrDefault(n => n.LanThi == id);
+            ViewBag.LanThi = dt.LanThi;
+            if (dt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(dt);
+        }
+
+        [HttpPost, ActionName("DTDelete")]
+        public ActionResult DTDeleteConfirm(int id)
+        {
+            DotThi dt = db.DotThis.SingleOrDefault(n => n.LanThi == id);
+            ViewBag.LanThi = dt.LanThi;
+            if (dt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.DotThis.DeleteOnSubmit(dt);
+            db.SubmitChanges();
+            return RedirectToAction("DT");
+        }
+
+        [HttpGet]
+        public ActionResult DTEdit(int id)
+        {
+            DotThi dt = db.DotThis.SingleOrDefault(n => n.LanThi == id);
+            ViewBag.LanThi = dt.LanThi;
+            if (dt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(dt);
+        }
+
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateInput(false)]
+        public ActionResult DTEdit(DotThi dt)
         {
-            try
-            {
-                // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
+            var d = db.DotThis.SingleOrDefault(n => n.LanThi == dt.LanThi);
+
+            if (ModelState.IsValid)
             {
-                return View();
+                d.LanThi = dt.LanThi;
+                d.NamThi = dt.NamThi;
+                d.NoiDung = dt.NoiDung;
+                db.SubmitChanges();
             }
+            return RedirectToAction("DT");
+        }
+        //// GET: Default/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Default/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Default/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Default/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Default/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Default/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        /*==================Giaithuong=====================*/
+
+        public ActionResult GT(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+
+            return View(db.GiaiThuongs.ToList().OrderBy(n => n.MaGT).ToPagedList(pageNumber, pageSize));
         }
 
-        // GET: Default/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public ActionResult GTCreate()
         {
             return View();
         }
 
-        // POST: Default/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult GTDetails(int id)
         {
-            try
+            GiaiThuong gt = db.GiaiThuongs.SingleOrDefault(n => n.MaGT == id);
+            ViewBag.MaGT = gt.MaGT;
+            if (gt == null)
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                Response.StatusCode = 404;
+                return null;
             }
-            catch
-            {
-                return View();
-            }
+            return View(gt);
         }
 
-        // GET: Default/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult GTDelete(int id)
+        {
+            GiaiThuong gt = db.GiaiThuongs.SingleOrDefault(n => n.MaGT == id);
+            ViewBag.MaGT = gt.MaGT;
+            if (gt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(gt);
+        }
+
+        [HttpPost, ActionName("GTDelete")]
+        public ActionResult GTDeleteConfirm(int id)
+        {
+            GiaiThuong gt = db.GiaiThuongs.SingleOrDefault(n => n.MaGT == id);
+            ViewBag.MaGT = gt.MaGT;
+            if (gt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.GiaiThuongs.DeleteOnSubmit(gt);
+            db.SubmitChanges();
+            return RedirectToAction("GT");
+        }
+
+        [HttpGet]
+        public ActionResult GTEdit(int id)
+        {
+            GiaiThuong gt = db.GiaiThuongs.SingleOrDefault(n => n.MaGT == id);
+            ViewBag.MaGT = gt.MaGT;
+            if (gt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(gt);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult GTEdit(GiaiThuong gt)
+        {
+
+            var g = db.GiaiThuongs.SingleOrDefault(n => n.MaGT == gt.MaGT);
+
+            if (ModelState.IsValid)
+            {
+                g.MaGT = gt.MaGT;
+                g.TenGiaiThuong = gt.TenGiaiThuong;
+                g.SoTienThuong = gt.SoTienThuong;
+                db.SubmitChanges();
+            }
+            return RedirectToAction("GT");
+        }
+
+        /*==================VongThi=====================*/
+
+        public ActionResult VT(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+
+            return View(db.VongThis.ToList().OrderBy(n => n.MaVT).ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpGet]
+        public ActionResult VTCreate()
         {
             return View();
         }
 
-        // POST: Default/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult VTDetails(int id)
         {
-            try
+            VongThi vt = db.VongThis.SingleOrDefault(n => n.MaVT == id);
+            ViewBag.MaVT = vt.MaVT;
+            if (vt == null)
             {
-                // TODO: Add delete logic here
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(vt);
+        }
 
-                return RedirectToAction("Index");
-            }
-            catch
+        public ActionResult VTDelete(int id)
+        {
+            VongThi vt = db.VongThis.SingleOrDefault(n => n.MaVT == id);
+            ViewBag.MaVT = vt.MaVT;
+            if (vt == null)
             {
-                return View();
+                Response.StatusCode = 404;
+                return null;
             }
+            return View(vt);
+        }
+
+        [HttpPost, ActionName("VTDelete")]
+        public ActionResult VTDeleteConfirm(int id)
+        {
+            VongThi vt = db.VongThis.SingleOrDefault(n => n.MaVT == id);
+            ViewBag.MaVT = vt.MaVT;
+            if (vt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.VongThis.DeleteOnSubmit(vt);
+            db.SubmitChanges();
+            return RedirectToAction("VT");
+        }
+
+        [HttpGet]
+        public ActionResult VTEdit(int id)
+        {
+            VongThi vt = db.VongThis.SingleOrDefault(n => n.MaVT == id);
+            ViewBag.MaVT = vt.MaVT;
+            if (vt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(vt);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult VTEdit(VongThi vt)
+        {
+
+            var v = db.VongThis.SingleOrDefault(n => n.MaVT == vt.MaVT);
+
+            if (ModelState.IsValid)
+            {
+                v.MaVT = vt.MaVT;
+                v.TenVT = vt.TenVT;
+                
+                db.SubmitChanges();
+            }
+            return RedirectToAction("VT");
         }
     }
 }
--- WebQLCTTHTH/WebQLCTTHTH/Controllers/BVDController.cs
+++ WebQLCTTHTH/WebQLCTTHTH/Controllers/BVDController.cs
@@ -3,15 +3,192 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebQLCTTHTH.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
 
 namespace WebQLCTTHTH.Controllers
 {
+
     public class BVDController : Controller
     {
+        QLCTTHTHDataContext db = new QLCTTHTHDataContext();
         // GET: BVD
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult NTT(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+
+            return View(db.NhaTaiTros.ToList().OrderBy(n => n.MaNTT).ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpGet]
+        public ActionResult NTTCreate()
+        {
+            return View();
+        }
+
+        public ActionResult NTTDetails(int id)
+        {
+            NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+            ViewBag.MaNTT = ntt.MaNTT;
+            if (ntt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(ntt);
+        }
+
+        public ActionResult NTTDelete(int id)
+        {
+            NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+            ViewBag.MaNTT = ntt.MaNTT;
+            if (ntt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(ntt);
+        }
+
+        [HttpPost, ActionName("NTTDelete")]
+        public ActionResult NTTDeleteConfirm(int id)
+        {
+            NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+            ViewBag.MaNTT = ntt.MaNTT;
+            if (ntt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.NhaTaiTros.DeleteOnSubmit(ntt);
+            db.SubmitChanges();
+            return RedirectToAction("NTT");
+        }
+
+        [HttpGet]
+        public ActionResult NTTEdit(int id)
+        {
+            NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+            ViewBag.MaNTT = ntt.MaNTT;
+            if (ntt == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(ntt);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult NTTEdit(NhaTaiTro ntt)
+        {
+
+            var t = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == ntt.MaNTT);
+
+            if (ModelState.IsValid)
+            {
+                t.MaNTT = ntt.MaNTT;
+                t.TenNTT = ntt.TenNTT;
+                t.DiaChiNTT = ntt.DiaChiNTT;
+                t.EmailNTT = ntt.EmailNTT;
+                db.SubmitChanges();
+            }
+            return RedirectToAction("NTT");
+        }
+
+        /*===========Chi Tiet Dot Thi=============*/
+
+        
+        //public ActionResult CTDT(int? page)
+        //{
+        //    int pageNumber = (page ?? 1);
+        //    int pageSize = 7;
+
+        //    return View(db.CTTTDTs.ToList().OrderBy(n => n.MaNTT).ToPagedList(pageNumber, pageSize));
+        //}
+
+        //[HttpGet]
+        //public ActionResult NTTCreate()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult NTTDetails(int id)
+        //{
+        //    NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+        //    ViewBag.MaNTT = ntt.MaNTT;
+        //    if (ntt == null)
+        //    {
+        //        Response.StatusCode = 404;
+        //        return null;
+        //    }
+        //    return View(ntt);
+        //}
+
+        //public ActionResult NTTDelete(int id)
+        //{
+        //    NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+        //    ViewBag.MaNTT = ntt.MaNTT;
+        //    if (ntt == null)
+        //    {
+        //        Response.StatusCode = 404;
+        //        return null;
+        //    }
+        //    return View(ntt);
+        //}
+
+        //[HttpPost, ActionName("NTTDelete")]
+        //public ActionResult NTTDeleteConfirm(int id)
+        //{
+        //    NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+        //    ViewBag.MaNTT = ntt.MaNTT;
+        //    if (ntt == null)
+        //    {
+        //        Response.StatusCode = 404;
+        //        return null;
+        //    }
+        //    db.NhaTaiTros.DeleteOnSubmit(ntt);
+        //    db.SubmitChanges();
+        //    return RedirectToAction("NTT");
+        //}
+
+        //[HttpGet]
+        //public ActionResult NTTEdit(int id)
+        //{
+        //    NhaTaiTro ntt = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == id);
+        //    ViewBag.MaNTT = ntt.MaNTT;
+        //    if (ntt == null)
+        //    {
+        //        Response.StatusCode = 404;
+        //        return null;
+        //    }
+        //    return View(ntt);
+        //}
+
+        //[HttpPost]
+        //[ValidateInput(false)]
+        //public ActionResult NTTEdit(NhaTaiTro ntt)
+        //{
+
+        //    var t = db.NhaTaiTros.SingleOrDefault(n => n.MaNTT == ntt.MaNTT);
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        t.MaNTT = ntt.MaNTT;
+        //        t.TenNTT = ntt.TenNTT;
+        //        t.DiaChiNTT = ntt.DiaChiNTT;
+        //        t.EmailNTT = ntt.EmailNTT;
+        //        db.SubmitChanges();
+        //    }
+        //    return RedirectToAction("NTT");
+        //}
     }
 }
